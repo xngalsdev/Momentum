29b4586a5fdd3349f5cc0e6a18e4ab3e13b34f09
Seungmin Song <142411910+xngalsdev@users.noreply.github.com>
2023-09-06 11:01:29 +0900

    Momentum 배경, 시간, 명언 나타내기

---
+++ 2023.9.6/clock.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector("h2#clock");
+
+/**
+ * 시각을 나타내는 함수
+ */
+function getclock(){
+    const date = new Date();
+
+    /*const hour = String(date.getHours()).padStart(2, '0');
+    const minute = String(date.getMinutes()).padStart(2, '0');
+    const second = String(date.getSeconds()).padStart(2, '0');
+    */
+    clock.innerText = (`${String(date.getHours()).padStart(2,'0')}:${String(date.getMinutes()).padStart(2,'0')}:${String(date.getSeconds()).padStart(2, '0')}`);
+}
+
+setInterval(getclock,1000);
